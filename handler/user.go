a7f00c499b3206f5cd3386e01566930260d14054
Masalief Maulana <m.maulana.fb26@gmail.com>
2024-01-29 11:46:51 +0700

    feat: Initial commit
    - Initial commit
    - Add user package with entity, input, repository, formatter, service, and handler files

---
+++ handler/user.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"govn-crowdfunding/helper"
+	"govn-crowdfunding/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *UserHandler {
+	return &UserHandler{userService}
+}
+
+func (h *UserHandler) RegisterUser(c *gin.Context) {
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return // agar tidak lanjut ke bawah
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return // agar tidak lanjut ke bawah
+	}
+
+	formatter := user.FormatUser(newUser, "tokentokentoken")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
+
+	// fmt.Println("%w", response)
+
+	c.JSON(http.StatusOK, response)
+}
